da5a5b1cd1e4cf1538aa14a5ddab0bd0b83f3e6b
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2024-05-12 17:36:13 +0300

    fix(mapper-dto): made PartyFacility navigation properties 'virtual'

--- src/Domain/Core/PartyFacility.cs
+++ src/Domain/Core/PartyFacility.cs
@@ -4,11 +4,11 @@
     {
         public int PartyId { get; set; }
 
-        public Party Party { get; set; } = null!;
+        public virtual Party Party { get; set; } = null!;
 
         public int FacilityId { get; set; }
 
-        public Facility Facility { get; set; } = null!;
+        public virtual Facility Facility { get; set; } = null!;
 
         public FacilityRoleTypeId FacilityRoleTypeId { get; set; }
 
