2f68856576db8f7c00304ad5cab5ab1c68c652d8
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2024-05-12 18:01:19 +0300

    fix(mapper-dto): fixed controllers to use mapper, added FacilitiesController

---
+++ src/WebAPI/Controllers/FacilitiesController.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using UtterlyComplete.ApplicationCore.Interfaces.Repositories;
+using UtterlyComplete.ApplicationCore.Models;
+using UtterlyComplete.Domain.Core;
+
+namespace WebAPI.Controllers
+{
+    public class FacilitiesController : BaseController<Facility, FacilityDto>
+    {
+        public FacilitiesController(IMapper mapper, ICrudRepository<Facility> repository) : base(mapper, repository)
+        {
+        }
+    }
+}
--- src/WebAPI/Controllers/PartiesController.cs
+++ src/WebAPI/Controllers/PartiesController.cs
@@ -1,11 +1,13 @@
 ﻿using UtterlyComplete.Domain.Core;
 using UtterlyComplete.ApplicationCore.Interfaces.Repositories;
+using UtterlyComplete.ApplicationCore.Models;
+using AutoMapper;
 
 namespace WebAPI.Controllers
 {
-    public class PartiesController : BaseController<Party>
+    public class PartiesController : BaseController<Party, PartyDto>
     {
-        public PartiesController(ICrudRepository<Party> repository) : base(repository)
+        public PartiesController(IMapper mapper, ICrudRepository<Party> repository) : base(mapper, repository)
         {
         }
     }
